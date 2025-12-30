565bd391526491436470dd38e208231657523983
CodingCatz <103620558+CodingCatz@users.noreply.github.com>
2025-12-30 16:26:33 +0000

    Deploying to gh-pages from @ hwudigidev-creator/web-game@b480197fe001a09c41f56ae488de6452531d476b 🚀

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for DIGI WAR PWA
-const CACHE_NAME = 'digi-war-v0.9.6a';
+const CACHE_NAME = 'digi-war-v0.9.7a';
 
 // 安裝 Service Worker
 self.addEventListener('install', (event) => {
